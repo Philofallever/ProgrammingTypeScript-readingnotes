2f716e8d4848bed1aa37308532dcd5bb913fa60b
Fallever <piscovip@163.com>
2021-10-26 23:53:58 +0800

    201/10/26
    Signed-off-by: Fallever <piscovip@163.com>

--- index.ts
+++ index.ts
@@ -1,5 +1,4 @@
-import { emit } from "process";
-
+/* 
 console.log("hello world");
 let a = 1 + 2;
 let b = a + 3;
@@ -143,14 +142,161 @@ class Catt implements Animal
 {
     eat(food: string): void
     {
-     
+
     }
     sleep(hours: number): void
     {
-      
+
     }
     age = 5;
 }
 
-let catt1 = (new Catt()).age
+let catt1 = (new Catt()).age;
 
+type State = {
+    [key: string]: string;
+};
+
+class StringDatabase
+{
+    state: State = {};
+
+    get(key: string): string | null
+    {
+        return key in this.state ? this.state[key] : null;
+    }
+
+    set(key: string, value: string)
+    {
+        this.state[key] = value;
+    }
+
+    static from(state: State)
+    {
+        let db = new StringDatabase;
+        for (let key in state)
+            db.set(key, state[key]);
+
+        return db;
+    }
+}
+
+let db = new StringDatabase()
+let info = typeof StringDatabase
+
+*/
+
+import { format } from "path/posix";
+
+/* type FriendList = {
+    count: number;
+    friends: {
+        firstName: string
+        lastName:string
+    }[]
+}
+
+type APIResponse = {
+    user: {
+        userId: string
+        friendList:FriendList
+    }
+}
+ */
+
+/* type APIResponse = {
+    user: {
+        userId: string;
+        friendList: {
+            count: number;
+            friends: {
+                firstName: string;
+                lastName: string;
+            }[];
+        };
+    };
+};
+
+type ResponseKeys = keyof APIResponse; // 'user'
+type UserKeys = keyof APIResponse['user'];  // 'userId'|friendList
+
+type Weekday = "Mon" | "Tue" | "Wed" | "Thu" | "Fri";
+type Day = Weekday | "Sat" | "Sun";
+
+let nextDay: Record<Weekday, Day> = {
+    Mon: 'Fri',
+    Tue: 'Mon',
+    Wed: 'Sun',
+    Thu: "Tue",
+    Fri: 'Sun'
+} */
+/* 
+type Weekday = "Mon" | "Tue" | "Wed" | "Thu" | "Fri";
+type Day = Weekday | "Sat" | "Sun";
+let nextDay: { [key in Weekday]: Day } = {
+    Mon: "Mon",
+    Tue: "Sat",
+    Wed: "Mon",
+    Thu: "Mon",
+    Fri: "Mon"
+};
+
+type Currency = {
+    unit: "EUR" | "GBP" | "JPY" | "USD";
+    value: number;
+};
+
+let Currency = {
+    DEFAULT: "USD",
+    from(value: number, unit = Currency.DEFAULT): Currency
+    {
+        return { unit, value };
+    }
+}; */
+
+/* function tuple<T extends unknown[]>( ...ts:T) :T
+{
+    return ts;
+}
+
+let a = [1, true]; //
+let b = tuple(1, true); //
+let c = [1, true] as const; // readonly [1,true]
+b[0] = 2; */
+
+/* function isString(a: unknown): a is string
+{
+    return typeof a === 'string';
+}
+
+function parseInput(input: string | number)
+{
+    if (isString(input))
+    {
+        return input.toUpperCase()    
+    }
+} */
+
+// type ToArray<T> = T extends unknown ? T[] : T[];
+// type A = ToArray<number>;
+// type B = ToArray<string | number>;
+
+// let b: B = [1, 2, 3, 4];
+
+
+type ElementType<T> = T extends unknown[] ? T[number] : T; //使用了键入
+type A = ElementType<number[]>;
+
+type ElementType2<T> = T extends (infer U)[] ? U : T;
+type B = ElementType<number[]>;
+
+type User = {
+    name: string;
+    id: number;
+};
+type C = Exclude<number | string, string>;
+type c = Partial<User>;
+type c1 = Readonly<User>;
+// import 
+
+type CompanyID = string & { readonly band :unique symbol; }
